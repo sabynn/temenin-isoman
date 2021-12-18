090aac5566776ea889cc135bdcc9f3478a6f7c6c
Sabyna Maharani <sabynamaharani@gmail.com>
2021-12-19 00:17:05 +0700

    add method for flutter integration

--- tips_and_tricks/urls.py
+++ tips_and_tricks/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index, add, load_more
+from .views import index, add, load_more, search_json, add_from_flutter
 
 
 urlpatterns = [
     path('', index, name='index_tips'),
     path('add', add, name='add_tips'),
-    path('load-more',load_more,name='load-more'),
+    path('load-more', load_more, name='load-more'),
+    path('search-json', search_json, name='search_json'),
+    path('add-from-flutter', add_from_flutter, name='add_from_flutter')
 ]
--- tips_and_tricks/views.py
+++ tips_and_tricks/views.py
@@ -1,3 +1,5 @@
+import json
+from django.views.decorators.csrf import csrf_exempt
 from main.decorators import *
 from django.shortcuts import render
 from django.core import serializers
@@ -7,15 +9,16 @@ from django.template.loader import render_to_string
 from tips_and_tricks.models import TipsAndTrick
 from tips_and_tricks.forms import AddForm
 
+
 def index(request):
     if 'q' in request.GET:
         q = request.GET['q']
-        articles=TipsAndTrick.objects.filter(title__icontains=q).order_by('id')
+        articles = TipsAndTrick.objects.filter(title__icontains=q).order_by('id')
         if q == '':
-            paginator=Paginator(articles, 3)
+            paginator = Paginator(articles, 3)
             articles = paginator.get_page(q)
     else:
-        articles=TipsAndTrick.objects.all().order_by('id')
+        articles = TipsAndTrick.objects.all().order_by('id')
 
     if request.is_ajax():
         html = render_to_string(
@@ -24,7 +27,7 @@ def index(request):
         data_dict = {"html_from_view": html}
         return JsonResponse(data=data_dict, safe=False)
 
-    paginator=Paginator(articles, 3)
+    paginator = Paginator(articles, 3)
     page_number = request.GET.get('page')
     posts_obj = paginator.get_page(page_number)
     response = {'articles': posts_obj, 'all_articles': articles}
@@ -41,13 +44,35 @@ def add(request):
 
 
 def load_more(request):
-    offset=int(request.GET['offset'])
-    limit=3
-    posts=TipsAndTrick.objects.all()[offset:limit+offset]
-    totalData=TipsAndTrick.objects.count()
-    data={}
-    posts_json=serializers.serialize('json',posts)
+    offset = int(request.GET['offset'])
+    limit = 3
+    posts = TipsAndTrick.objects.all()[offset:limit + offset]
+    totalData = TipsAndTrick.objects.count()
+    data = {}
+    posts_json = serializers.serialize('json', posts)
     return JsonResponse(data={
-        'posts':posts_json,
-        'totalResult':totalData
+        'posts': posts_json,
+        'totalResult': totalData
+    })
+
+
+def search_json(request):
+    if 'q' in request.GET:
+        q = request.GET['q']
+        articles = TipsAndTrick.objects.filter(title__icontains=q).order_by('id')
+    else:
+        articles = TipsAndTrick.objects.all().order_by('id')
+    data = serializers.serialize('json', articles)
+    return HttpResponse(data, content_type="application/json")
+
+
+@csrf_exempt
+def add_from_flutter(request):
+    body_unicode = request.body.decode('utf-8')
+    data = json.loads(body_unicode)
+    new_article = TipsAndTrick(**data)
+    new_article.save()
+    print("YEAY INI DATANYA:", data)
+    return JsonResponse({
+        "success": "New Article Successfully Added",
     })
